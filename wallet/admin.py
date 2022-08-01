624c4f00bfbb3b8118098aeb25e3cecedb6948b8
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-08-01 12:58:07 +0300

    Digital wallet

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,16 +1,10 @@
-from tkinter import N
-from django.contrib import admin
 
+from django.contrib import admin
 # Register your models here.
 from .models import Account, Card, Customer, Loan, Notification, Receipt, Reward, ThirdParty, Transaction, Wallet
- 
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display=("first_name","last_name","age","email")
-    search_fields=("first_name","last_name")
-admin.site.register(Customer,CustomerAdmin)
-admin.site.register(Wallet)
+admin. site.register(Customer)
 admin.site.register(Account)
+admin.site.register(Wallet)
 admin.site.register(Transaction)
 admin.site.register(Card)
 admin.site.register(ThirdParty)
